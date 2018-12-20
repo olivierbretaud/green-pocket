cd0dddeaae239977a12fcb234335aaa693a4e3a6
Olivier Bretaud <olivierbretaud@hotmail.fr>
2018-12-20 11:55:01 +0100

    scroll effect

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="main-container">
-    <Question01 />
+  <start id="00"/>
+  <Question01 id="01"/>
   </div>
 </template>
 
 <script>
 import Question01 from './components/Question01.vue'
+import Start from './components/Start.vue'
+
 
 export default {
   name: 'app',
   components: {
-    Question01
-  }
+    Question01,
+    Start
+  },
 }
+
+$(document).ready(function() {
+$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+var page = $(this).attr('href'); // Page cible
+var speed = 750; // Durée de l'animation (en ms)
+$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+return false;
+
+});
+
+});
+
+
 </script>
 
 <style>
@@ -24,4 +41,5 @@ body {
   color: whitesmoke;
   background-color: #16b164;
 }
+
 </style>
--- src/components/Question01.vue
+++ src/components/Question01.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid d-flex flex-column justify-content-center align-items-center">
-    <div class="awnser size-anim" v-if="displayAwnser">
+    <div class="awnser size-anim" v-if="displayAwnser01">
       <svg id="bg-anim" width="100%" height="100%" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <rect id="a" x="-2" y="6" width="10" height="1" rx="0.5" ry="0.5" fill="#000" >
           <animateMotion
@@ -18,13 +18,14 @@
       </svg>
     </div>
     <div class="question d-flex flex-column align-items-center">
-      <h4 class="font-weight-bold" v-if="goodAwnser">Bravo!</h4>
-      <h4 class="font-weight-bold" v-if="badAwnser">Mauvaise réponse</h4>
-      <h5 class="m-3" v-if="!displayAwnser" >{{ question }}</h5>
-      <h5 class="m-3" v-if="displayAwnser" >{{ solution }}</h5>
+      <h4 class="font-weight-bold" v-if="goodAwnser01">Bravo!</h4>
+      <h4 class="font-weight-bold" v-if="badAwnser01">Mauvaise réponse</h4>
+      <h5 class="m-3" v-if="!displayAwnser01" >{{ question01 }}</h5>
+      <h5 class="m-3" v-if="displayAwnser01" >{{ solution01 }}</h5>
       <div>
-        <button type="button" class="btn btn-light m-2" v-bind:class="{ 'd-none' : displayAwnser}" v-on:click="displayAwnser = true" @click="goodAwnser = true"> {{ awnserA }} </button>
-        <button type="button" class="btn btn-light m-2" v-bind:class="{ 'd-none': displayAwnser}" v-on:click="displayAwnser = true" @click="badAwnser = true"> {{ awnserB }} </button>
+        <button type="button" class="btn btn-light m-2" v-bind:class="{ 'd-none' : displayAwnser01}" v-on:click="displayAwnser01 = true" @click="goodAwnser01 = true"> {{ awnserA01 }} </button>
+        <button type="button" class="btn btn-light m-2" v-bind:class="{ 'd-none': displayAwnser01}" v-on:click="displayAwnser01 = true" @click="badAwnser01 = true"> {{ awnserB01 }} </button>
+        <a a href="#00" class="js-scrollTo"><button type="button" class="btn btn-light m-2" v-if="displayAwnser01" >Question suivante</button></a>
       </div>
     </div>
   </div>
@@ -35,13 +36,13 @@ export default {
   name: 'Question01',
   data() {
     return {
-      question: 'La pollution issue de l’usage d’un téléphone est elle plus importante que celle issue de sa fabrication ?',
-      solution: "La pollution issue de la production un téléphone est bien plus importante, l’extraction des minerais qui le composent à impact considérable sur l’environnement",
-      awnserA: 'oui',
-      awnserB: 'non',
-      displayAwnser: false,
-      goodAwnser: false,
-      badAwnser: false,
+      question01: 'La pollution issue de l’usage d’un téléphone est elle plus importante que celle issue de sa fabrication ?',
+      solution01: "La pollution issue de la production un téléphone est bien plus importante, l’extraction des minerais qui le composent à impact considérable sur l’environnement",
+      awnserA01: 'oui',
+      awnserB01: 'non',
+      displayAwnser01: false,
+      goodAwnser01: false,
+      badAwnser01: false,
     }
   }
 }
@@ -60,7 +61,7 @@ export default {
 }
 
 .awnser {
-  width: 100vw;
+  width: 96vw;
   height: 60vh;
 }
 
@@ -89,7 +90,7 @@ export default {
     opacity: 0;
   }
   to {
-    width: 100vw;
+    width: 96vw;
     height: 60vh;
     opacity: 1;
   }
@@ -103,7 +104,7 @@ export default {
     opacity: 0;
   }
   to {
-    width: 100vw;
+    width: 96vw;
     height: 60vh;
     opacity: 1;
   }
